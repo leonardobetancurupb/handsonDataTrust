61a16485c4e9dddc7da18deb43bea96c88b100c2
018monroz <juan.montoyao@upb.edu.co>
2024-06-11 22:03:17 -0500

    Base logger files

---
+++ auditoria/calls.py
@@ -0,0 +1,67 @@
+import requests
+import json
+import time
+import hashlib
+
+
+# Example data
+type = "query"
+source = "user_1"
+destination = "rds_db_1"
+description = {"query":"SELECT * from TABLE personas"}
+
+def send_log(type,content,source,destination):
+
+  url = "http://127.0.0.1:5000/response"
+  
+  payload = {
+      
+    "type" : type,
+    "from" : source,
+    "to"   : destination,
+    "content" : content
+  }
+
+  def generate_token():
+    token = "PALABRA"+str(payload)+time.strftime("%Y-%m-%d", time.localtime())
+    token = hashlib.sha256(token.encode()).hexdigest()
+    return token
+
+  generated_token = generate_token()
+
+  headers = {
+  'Content-Type': 'application/json',
+  'Authorization': f'Bearer {generated_token}'
+  }
+
+  response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+
+  return response.text
+
+def validate_log():
+    
+  url = "http://127.0.0.1:5000/validate"
+  
+  payload = {}
+
+  def generate_token():
+    token = "PALABRA"+str(payload)+time.strftime("%Y-%m-%d", time.localtime())
+    token = hashlib.sha256(token.encode()).hexdigest()
+    return token
+
+  generated_token = generate_token()
+
+  headers = {
+  'Content-Type': 'application/json',
+  'Authorization': f'Bearer {generated_token}'
+  }
+
+  response = requests.request("GET", url, headers=headers, data=json.dumps(payload))
+
+  return response.text
+
+
+# Sample calls
+send_log(type,description,source,destination)
+
+validate_log()
---
+++ auditoria/test.py
@@ -0,0 +1,163 @@
+import json
+import time
+import hashlib
+from flask import Flask, jsonify, request
+
+# Function: Add log to file
+def add_log(content, source, destination, type):
+
+        # Last log reference needed for hashing validation
+        last_log = get_last_log(file)
+
+        if last_log:
+            last_id = last_log['log_id']
+            last_hash = last_log['actual_hash']
+        else:
+            last_id = 0
+            last_hash = hashlib.sha256(str(time.time()).encode()).hexdigest()
+
+        # Log structure
+        log  = {
+            "log_id" : last_id + 1,
+            "timestamp" : time.time(),
+            "source" : source,
+            "destination" : destination,
+            "type" : type,
+            "description" : content,
+            "last_hash" : last_hash
+        }
+
+        # Calculating actual hash and adding to log
+        actual_hash = hashlib.sha256(str(log).encode()).hexdigest()
+        log['actual_hash'] = actual_hash
+
+        # Saving the log
+        with open(file, 'a') as f:
+            f.write(json.dumps(log) + '\n')
+
+        print(f"log archived")
+
+# Function: Get last log from file
+def get_last_log(file):
+
+    with open(file, 'r') as f:
+
+        logs = f.readlines()
+        if not logs:
+            print("Empty file.")
+            return None
+        last_log = logs[-1]
+        return json.loads(last_log)
+
+def validate_log(file):
+
+    with open(file, 'r') as f:
+
+        logs = f.readlines()
+        if not logs:
+            print("Empty file.")
+            return None
+        
+        last_hash = None
+        
+        for registry in logs:
+            log = json.loads(registry)
+
+            #validation on registered log equals calculated log
+            registered_hash = log.pop('actual_hash')
+            actual_hash = hashlib.sha256(str(log).encode()).hexdigest()
+
+            if actual_hash != registered_hash:
+                return f"Error on log: {log['log_id']}"
+            
+            #validation on previous hash 
+            if registry != logs[0] and registry != logs[-1]:
+
+                if last_hash != log['last_hash']:
+                    return f"Error on log: {log['log_id']}"
+            
+            last_hash = actual_hash
+        
+        return "Consistent Log!"
+
+def validate_token(payload):
+
+    content = json.loads(payload)
+    auth_header = request.headers.get('Authorization')
+
+    # Token validation process
+    if auth_header and auth_header.startswith('Bearer '):
+        token = auth_header.split(' ')[1]
+        print("Token: "+token)
+    else:
+        print("Token not found")
+        return False
+
+    text = "PALABRA"+str(content)+time.strftime("%Y-%m-%d", time.localtime())
+    text = hashlib.sha256(text.encode()).hexdigest()
+
+    if text == token:
+        return True
+    
+    return False
+
+app = Flask(__name__)
+
+# Log file path
+file = "audit.txt"
+
+@app.route('/response', methods=['POST'])
+def reply():
+
+
+    payload = request.get_data(as_text=True)
+    content = json.loads(payload)
+
+    if validate_token(payload):
+
+        add_log(content,content.pop('from'), content.pop('to'), content.pop('type'))
+
+        response = {
+            'auth': 'Token validated',
+            'message': 'Log registered.'
+        }
+
+        return jsonify(response), 201
+
+    else:
+
+        response = {
+            'auth': 'Invalid Token :(',
+            'message': 'Unable to log.'
+        }
+
+        return jsonify(response), 401
+    
+
+
+@app.route('/validate', methods=['GET'])
+def validation_reply():
+
+    payload = request.get_data(as_text=True)
+
+    if validate_token(payload):
+    
+        status = validate_log(file)
+
+        response = {'auth' : 'Token validated', 'log_status' : status} 
+
+    else: response = {'auth' : 'Invalid token :(', 'log_status' : " null "} 
+
+    return jsonify(response), 201
+
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port)
