dc285f0c602bd7573bd3d5933c0834c1da22e93b
luhbello <luisa.alvabello@gmail.com>
2024-08-22 03:52:50 -0500

    v40 reload path

--- frontend/main/administrator/views.py
+++ frontend/main/administrator/views.py
@@ -10,7 +10,7 @@ from django.core.cache import cache
 from django.conf import settings
 
 
-with open('../src/utils/key.txt', 'r') as file:
+with open('/code/src/utils/key.txt', 'r') as file:
     key = file.read()
 # Create your views here.
 def menu(request):
